64beb9a8bb321697eec9de0d27f8471b21928440
Mikkel <mikkellars@hotmail.com>
2020-10-14 13:21:28 +0200

    init camera_control for showing img from robot

---
+++ nodes/camera_control/src/camera_control_node.cpp
@@ -0,0 +1,12 @@
+#include <ros/ros.h>
+#include "camera_control/camera_control.hpp"
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "camera_control");
+  ros::NodeHandle nodeHandle("~");
+  camera_control::camera_control camera_control(nodeHandle);
+
+  ros::spin();
+  return 0;
+}
