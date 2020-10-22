7743cdbb78e17f518da7fbc89babeb39160947aa
Mikkel <mikkellars@hotmail.com>
2020-10-22 08:50:50 +0200

    init branch camera_save

---
+++ nodes/camera_control/include/camera_control/image_save.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <opencv2/highgui/highgui.hpp>
+
+namespace image_save {
+/*!
+ * Class containing the image save to save images comming from the camera
+ */
+class image_save {
+public:
+  /*!
+   * Constructor.
+   */
+  image_save(cv::Mat img);
+  /*!
+   * Destructor.
+   */
+  virtual ~image_save();
+
+private:
+
+};
+
+} /* namespace */
---
+++ nodes/camera_control/src/image_save.cpp
@@ -0,0 +1,14 @@
+#include "camera_control/image_save.hpp"
+
+namespace image_save {
+
+image_save::image_save(cv::Mat img)
+{
+
+}
+
+image_save::~image_save()
+{
+}
+
+} /* namespace */
