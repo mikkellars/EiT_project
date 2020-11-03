b42b94593ba523c9f5f1353f65d81c66c32ae2bd
Mikkel <mikkellars@hotmail.com>
2020-11-03 22:00:10 +0100

    modified gitignore to ignore pycharm

--- vision/texel_resnet50/train.py
+++ vision/texel_resnet50/train.py
@@ -1,19 +1,49 @@
-import tensorflow as tf
+"""
+Training of ResNet50 for texel dataset.
+"""
 
-path = "/home/mikkel/Documents/experts_in_teams_proj/vision/data/fence_data/texel_data"
+from fastai.vision.all import *
 
-datagen = tf.keras.preprocessing.image_dataset_from_directory(
-    path,
-    labels='inferred',
-    label_mode='binary',
-    color_mode="rgb",
-    batch_size=32,
-    image_size=(128, 128),
-    shuffle=True,
-    seed=None,
-    validation_split=None,
-    subset=None,
-    interpolation="bilinear",
-    follow_links=False,
-)
+def parse_arguments():
+    import argparse
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--model_name', type=str, default='model', help='name of trained model')
+    parser.add_argument('--data_root', type=str,
+                        default='/home/mikkel/Documents/data/scape_data/LUK3-L-02204-0G07-20-Bin2/glico_gen/baseline_crop',
+                        help='path to dataset either downloaded or to be downloaded')
+    parser.add_argument('--workers', type=int, default=8, help='number of data loading workers (default: 8)')
+    parser.add_argument('--save_dir_model', type=str, default='baseline/resnet50/models', help='path to save models')
+    parser.add_argument('--save_dir_logs', type=str, default='baseline/resnet50/logs', help='logs path for tensorboard')
+    parser.add_argument('--load_model_param', type=str, default='baseline/resnet50/hparams.pt')
+    parser.add_argument('--epochs', type=int, default=100, help='number of epochs (default: 100)')
+    args = parser.parse_args()
+    return args
 
+
+def main(args):
+    start_time = time.time()
+
+    # Set device
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    DATASET_PATH = Path("/home/mikkel/Documents/experts_in_teams_proj/vision/data/fence_data/texel_data")
+
+    mask_datablock = DataBlock(
+        get_items=get_image_files,
+        get_y=parent_label,
+        blocks=(ImageBlock, CategoryBlock),
+        item_tfms=RandomResizedCrop(224, min_scale=0.3),
+        splitter=RandomSplitter(valid_pct=0.2, seed=100),
+        batch_tfms=aug_transforms(mult=2)
+    )
+
+    dls = mask_datablock.dataloaders(DATASET_PATH)
+
+    learn = cnn_learner(dls, resnet50, metrics=error_rate)
+    learn.fine_tune(4)
+
+
+if __name__ == '__main__':
+    print(__doc__)
+    args = parse_arguments()
+    main(args)
