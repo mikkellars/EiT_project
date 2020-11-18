c32b4f2650949b8b8b5c02e83cba5c63b54564cd
mikkel <mikkellars@hotmail.com>
2020-11-18 08:38:34 +0100

    trained model

--- vision/tf_object_detection/model_main.py
+++ vision/tf_object_detection/model_main.py
@@ -59,7 +59,7 @@ FLAGS = flags.FLAGS
 def main(unused_argv):
   flags.mark_flag_as_required('model_dir')
   flags.mark_flag_as_required('pipeline_config_path')
-  config = tf.estimator.RunConfig(model_dir=FLAGS.model_dir, keep_checkpoint_max=None)
+  config = tf.estimator.RunConfig(model_dir=FLAGS.model_dir, keep_checkpoint_max=0)
   train_and_eval_dict = model_lib.create_estimator_and_inputs(
       run_config=config,
       pipeline_config_path=FLAGS.pipeline_config_path,
