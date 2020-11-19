d2efedcf64e754b674e4d218c5d0420e0a6c1e9b
mikkel <mikkellars@hotmail.com>
2020-11-19 11:55:05 +0100

    detection node

--- nodes/detection/src/tflite_inference/tflite_inference.py
+++ nodes/detection/src/tflite_inference/tflite_inference.py
@@ -1,7 +1,14 @@
 """Python file for running trained tflite model
 """
 import rospy
+from pycoral.utils import edgetpu
+
 
 class tfliteInference():
-    def __init__(self, model_path:str, ):
-        
+    def __init__(self, model_path:str):
+        self.model_path = model_path
+        self.interpreter = edgetpu.make_interpreter(model_file)
+
+    
+    def init_interpreter(self):
+        self.interpreter.allocate_tensors()
