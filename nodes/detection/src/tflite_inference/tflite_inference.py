c6ff56db8952239e4e1f2d80c081789577b2e182
mikkel <mikkellars@hotmail.com>
2020-12-04 15:29:43 +0100

    simulation not working

--- nodes/detection/src/tflite_inference/tflite_inference.py
+++ nodes/detection/src/tflite_inference/tflite_inference.py
@@ -1,13 +1,18 @@
 """Python file for running trained tflite model
 """
+# ROS
 import rospy
+from sensor_msgs.msg import Image
+
+import cv2
 import importlib.util
 
 
 
 class tfliteInference():
-    def __init__(self, model_path:str, use_TPU:bool = False):
+    def __init__(self, model_path:str, sub_img_name:str, use_TPU:bool = False):
         self.model_path = model_path
+        self.sub_img_name = sub_img_name
         self.use_TPU = use_TPU
         self.interpreter = self.__init_interpreter()
 
@@ -16,6 +21,9 @@ class tfliteInference():
         self.output_details = self.interpreter.get_output_details()
         self.height = self.input_details[0]['shape'][1]
         self.width = self.input_details[0]['shape'][2]
+
+        # Running subriber and inference
+        self.subscriber = rospy.Subscriber(self.sub_img_name, Image, self.callback,  queue_size = 1)
 
     
     def __init_interpreter(self):
@@ -49,3 +57,12 @@ class tfliteInference():
         num_detections = interpreter.get_tensor(self.output_details[3]['index'])[0]
 
         return boxes, classes, scores, num_detections
+
+    def callback(self, ros_data):
+        # conversion to CV2 
+        np_arr = np.fromstring(ros_data.data, np.uint8)
+        image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+        
+        cv2.imshow('cv_img', image_np)
+        cv2.waitKey(2)
+  
