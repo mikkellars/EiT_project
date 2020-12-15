47ff966a805a09524158b46dd41c7f641546a1c9
mikkel <mikkellars@hotmail.com>
2020-12-15 20:31:32 +0100

    working in sim and forbit

--- docker/ros-fence-inspection/README.md
+++ docker/ros-fence-inspection/README.md
@@ -1,4 +1,4 @@
-# Running the docker after build
+# Running the docker after build to not show images from cv
 docker run --cpuset-cpus 0 --network host --name ros-fence-inspection --privileged -v /dev/bus/usb:/dev/bus/usb --mount type=bind,source="$(pwd)"/assets,target=/assets --rm -it ros-fence-inspection
 
 # First run if on new terminal
@@ -8,4 +8,3 @@ xhost +
 # Then to run the docker
 
 docker run --cpuset-cpus 0 --network host --name ros-fence-inspection --privileged -v /tmp/.X11-unix:/tmp/.X11-unix -v /dev/bus/usb:/dev/bus/usb -e DISPLAY=$DISPLAY --mount type=bind,source="$(pwd)"/assets,target=/assets --rm -it ros-fence-inspection
-s
--- docker/ros-fence-inspection/nodes/ico_fence_following/bin/ico_fence_following.py
+++ docker/ros-fence-inspection/nodes/ico_fence_following/bin/ico_fence_following.py
@@ -6,19 +6,24 @@ from ico.learn_and_follow import *#LearnFollow
 def main():
 
     rospy.init_node('ico_fence_following', anonymous=True)
-    rospy.Rate(10)
+    rospy.Rate(50)
 
-  
+    if not rospy.has_param('~simulate'):
+        ValueError('Need to set simulate param')
+
+    simulate = rospy.get_param('~simulate')
+    
+    if simulate: # Simulate
+        pub_name_mc = ['/cmd_vel']
+    else: # Frobit
+        pub_name_mc = ['/frobit/left_pwm', '/frobit/right_pwm']
+
     sub_name = 'laser/dist_to_wall' 
-   # pub_name = '/frobit/twist' # Simulation
-
-    pub_name_left_mc = '/frobit/left_pwm' # Frobit
-    pub_name_right_mc = '/frobit/right_pwm' # Frobit
 
     target_dist = 1.0 # Meters
     learn_inteval = 0.2 # Meters: Accepted interval without learning
 
-    learn_follow = LearnFollow(sub_name, pub_name_left_mc, pub_name_right_mc, target_dist, learn_inteval)
+    learn_follow = LearnFollow(sub_name, pub_name_mc, target_dist, learn_inteval, simulate)
 
     try:
         rospy.spin()
